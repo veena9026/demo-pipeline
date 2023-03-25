395a3a9aa212393707ddff39e051fb97be87a131
ashu30348 <ashok1240408@gmail.com>
2023-03-25 18:08:25 +0530

    all commit are added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup ,find_packages
+
+setup(name="Cenus-income",
+      version="0.0.1",
+      author="veena",
+      author_email="veenakumar06@gmail.com",
+      packages=find_packages(),
+      install_requires=["pandas","numpy","flask"]
+      )
